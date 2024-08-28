17eb25bc7d6c900342b2973f916bcd6ea545ce60
Dai-Wenxun <wxDai2001@gmail.com>
2024-08-28 13:42:00 +0800

    refactor TransformerEncoderLayer

--- mld/models/operator/attention.py
+++ mld/models/operator/attention.py
@@ -222,10 +222,9 @@ class TransformerEncoderLayer(nn.Module):
 
     def __init__(self, d_model: int, nhead: int, dim_feedforward: int = 2048, dropout: float = 0.1,
                  activation: str = "relu", normalize_before: bool = False) -> None:
-        super().__init__()
+        super(TransformerEncoderLayer, self).__init__()
         self.d_model = d_model
         self.self_attn = nn.MultiheadAttention(d_model, nhead, dropout=dropout)
-        # Implementation of Feedforward model
         self.linear1 = nn.Linear(d_model, dim_feedforward)
         self.dropout = nn.Dropout(dropout)
         self.linear2 = nn.Linear(dim_feedforward, d_model)
@@ -238,18 +237,12 @@ class TransformerEncoderLayer(nn.Module):
         self.activation = _get_activation_fn(activation)
         self.normalize_before = normalize_before
 
-    def with_pos_embed(self, tensor: torch.Tensor, pos: Optional[Tensor] = None) -> torch.Tensor:
-        return tensor if pos is None else tensor + pos
-
     def forward_post(self,
                      src: torch.Tensor,
                      src_mask: Optional[torch.Tensor] = None,
-                     src_key_padding_mask: Optional[torch.Tensor] = None,
-                     pos: Optional[torch.Tensor] = None) -> torch.Tensor:
-
-        q = k = self.with_pos_embed(src, pos)
-        src2 = self.self_attn(q, k, value=src, attn_mask=src_mask,
-                              key_padding_mask=src_key_padding_mask)[0]
+                     src_key_padding_mask: Optional[torch.Tensor] = None) -> torch.Tensor:
+        q = k = v = src
+        src2 = self.self_attn(q, k, value=v, attn_mask=src_mask, key_padding_mask=src_key_padding_mask)[0]
         src = src + self.dropout1(src2)
         src = self.norm1(src)
         src2 = self.linear2(self.dropout(self.activation(self.linear1(src))))
@@ -260,13 +253,11 @@ class TransformerEncoderLayer(nn.Module):
     def forward_pre(self,
                     src: torch.Tensor,
                     src_mask: Optional[torch.Tensor] = None,
-                    src_key_padding_mask: Optional[torch.Tensor] = None,
-                    pos: Optional[torch.Tensor] = None) -> torch.Tensor:
+                    src_key_padding_mask: Optional[torch.Tensor] = None) -> torch.Tensor:
 
         src2 = self.norm1(src)
-        q = k = self.with_pos_embed(src2, pos)
-        src2 = self.self_attn(q, k, value=src2, attn_mask=src_mask,
-                              key_padding_mask=src_key_padding_mask)[0]
+        q = k = v = src2
+        src2 = self.self_attn(q, k, value=v, attn_mask=src_mask, key_padding_mask=src_key_padding_mask)[0]
         src = src + self.dropout1(src2)
         src2 = self.norm2(src)
         src2 = self.linear2(self.dropout(self.activation(self.linear1(src2))))
@@ -276,11 +267,10 @@ class TransformerEncoderLayer(nn.Module):
     def forward(self,
                 src: torch.Tensor,
                 src_mask: Optional[torch.Tensor] = None,
-                src_key_padding_mask: Optional[torch.Tensor] = None,
-                pos: Optional[torch.Tensor] = None) -> torch.Tensor:
+                src_key_padding_mask: Optional[torch.Tensor] = None) -> torch.Tensor:
         if self.normalize_before:
-            return self.forward_pre(src, src_mask, src_key_padding_mask, pos)
-        return self.forward_post(src, src_mask, src_key_padding_mask, pos)
+            return self.forward_pre(src, src_mask, src_key_padding_mask)
+        return self.forward_post(src, src_mask, src_key_padding_mask)
 
 
 class TransformerDecoderLayer(nn.Module):
