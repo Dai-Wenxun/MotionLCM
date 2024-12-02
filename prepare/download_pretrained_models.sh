0d08cf106cbb5eda6e91c9ec2c774c9ef9eda637
Dai-Wenxun <wxDai2001@gmail.com>
2024-12-02 21:04:07 +0800

    fix

--- prepare/download_pretrained_models.sh
+++ prepare/download_pretrained_models.sh
@@ -1,13 +1,16 @@
+echo -e "Downloading experiments_recons!"
+gdown --fuzzy https://drive.google.com/file/d/15zFDitcOLhjbQ0CaOoM-QNKQUeyJw-Om/view?usp=sharing
+unzip experiments_recons.zip
+
 echo -e "Downloading experiments_t2m!"
-# experiments_t2m
-gdown --fuzzy https://drive.google.com/file/d/1RpNumy8g2X2lI4H-kO9xkZVYRDDgyu6Y/view?usp=sharing
+gdown --fuzzy https://drive.google.com/file/d/1U7homKobR2gaDLfL5flS3N0g7e0a_AQd/view?usp=sharing
 unzip experiments_t2m.zip
 
 echo -e "Downloading experiments_control!"
-# experiments_control
-gdown --fuzzy https://drive.google.com/file/d/1_xrSGWHo4pRz-AJj22rt8qmRX0SflJ6V/view?usp=sharing
+gdown --fuzzy https://drive.google.com/file/d/13NiRg0xpd2aDAG0ZNu1nenJ6ElkDVmYI/view?usp=sharing
 unzip experiments_control.zip
 
+rm experiments_recons.zip
 rm experiments_t2m.zip
 rm experiments_control.zip
 
